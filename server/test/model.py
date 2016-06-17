0fe4e270271f46e8122a28b75959aa7e743d8ae5
Gautham Varadarajan <gauthamvaradarajan@gmail.com>
2016-06-17 15:04:20 -0400

    createchampion test passed

--- server/test/model.py
+++ server/test/model.py
@@ -1,15 +1,22 @@
-from model import Base
-from model import Champion
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import unittest
+import imp
+
+
+champion_model = imp.load_source('champion', '../src/models/champion.py')
+Champion = champion_model.Champion
+Base = champion_model.Base
 
 # to be able to run the following tests, create a user in Postgres
 # called 'metarifttester' and a database called 'metarifttest' to which
-# metarifttester has access
+# metarifttester has access, then replace 'password' with the user's password
 
 
-engine = create_engine("postgresql://falcontester:metarifttester@localhost/metarifttest")
+engine = create_engine("postgresql://metarifttester:password@localhost/metarifttest")
+
+Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 
@@ -18,6 +25,12 @@ class ModelTestMethods(unittest.TestCase):
     def test_createchampion(self):
         test_session = Session()
         fake_champion = Champion(name="Fake", role="11011")
-        session.add(fake_champion)
-        test_champion = session.query(Champion).filter_by(name="Fake").first()
+        test_session.add(fake_champion)
+        test_champion = test_session.query(Champion).filter_by(name="Fake").first()
         self.assertTrue(fake_champion is test_champion)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
