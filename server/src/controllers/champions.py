6ffb38b1e019dc7f4f64e4680d870541c52470a5
Gautham Varadarajan <gauthamvaradarajan@gmail.com>
2016-06-21 18:10:20 -0400

    create first controller with champion show

--- server/src/controllers/champions.py
+++ server/src/controllers/champions.py
@@ -7,3 +7,25 @@ import imp
 champion_model = imp.load_source('champion', '../models/champion.py')
 Champion = champion_model.Champion
 Base = champion_model.Base
+
+engine = create_engine("postgresql://metarift:leagueofpidgeons@localhost/metariftdevelopment")
+
+Session = sessionmaker(bind=engine)
+
+class ChampionShow():
+
+    def on_get(self, req, resp, name):
+        show_session = Session()
+        hero = show_session.query(Champion).filter(Champion.name == name).one_or_none()
+        if hero == None:
+            resp.body = "No such hero found"
+        else:
+            hero_object = {'id': hero.id, 
+                           'name': hero.name,
+                           'role': hero.role,
+                           'win_rate': hero.win_rate,
+                           'ban_rate': hero.ban_rate,
+                           'pick_rate': hero.pick_rate,
+                           'image': hero.image_url
+                           }
+            resp.data = json.dumps(hero_object)
